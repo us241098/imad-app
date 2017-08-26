cc52bdc618491f4766fb3ddc751306fcbe3644d7
Utsav Shukla <us241077@gmail.com>
2017-08-27 02:38:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get("/counter",function(req,res)){
+    counter = counter+1;
+    res.send(coounter.tostring());
+    
+});
+
 app.get('/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 

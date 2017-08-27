1feec356add8f1de877cae8bc01d25cafe783880
Utsav Shukla <us241077@gmail.com>
2017-08-27 18:45:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+//COUNTER CODE
+var button = document.getElementById('counter');
+var counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+};

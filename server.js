dd72418964532c64d3af51dc49794b0f8d4d5212
Utsav Shukla <us241077@gmail.com>
2017-09-10 01:21:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/counter',function(req,res){
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui'article-one.html'));
+  res.sendFile(path.join(__dirname,'ui','article-one.html'));
 
 });
 

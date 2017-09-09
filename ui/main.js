55aaaad4759d9be93bcbc0ce1d118c6dbf88b100
Utsav Shukla <us241077@gmail.com>
2017-09-10 03:56:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,12 @@ var button = document.getElementById('counter');
 button.onclick = function () {
     var request = new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readyState==XMLHttpRequest.DONE){
+        if(request.readyState == XMLHttpRequest.DONE){
             
-            if(request.status==200){
+             if(request.status==200){
                 var counter = request.responseText;
                 var span=document.getElementById('count');
-                span.innerhtml = counter.toString();
+                span.innerHTML = counter.toString();
                 
             }
         }
